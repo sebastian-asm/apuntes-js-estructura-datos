3cc671601768ebf956f9185e767544fca3742c55
Sebastian Sanchez <sebastian.asm@outlook.com>
2024-01-04 20:30:14 -0300

    arrays

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 La estructuras de datos sirven para guardar información. En JS casi todo es un objecto (entidad abstracta que representa cualquier cosa) a excepción de los tipos primitivos: string, number, bigint, boolean, undefined y symbol. Con los objectos, _los valores se pasan por referencia_, esto quiere decir que si se modifica el objecto original o su copia esto afectará los valores de ambos. Diferente es el comportamiento de los primitivos que solo se pasa el valor y no su referencia.
 
-## Class
+## Clases
 
 Son un "molde" para crear objetos.
 
 - **Constructor**: método que se invoca al momento de crear un objecto
 - **Super**: palabra clave para invocar al constructor de la clase padre
 - **Herencia**: en la programación POO es cuando se heredan propiedades y métodos de otras clases, para expandir y reutilizar funcionalidades
+
+## Programación funcional
+
+JS permite el paradigma de la programación funcional, que se basa solo en funciones, aunque no al 100%. En JS existen varios métodos mutables e inmutables (algo que no se modifica/edita). Por ejemplo, _push_ de los arrays en un método mutable ya que altera el original pero _filter_ o _map_ son inmutables, devuelve un nuevo array y no cambian el original.
+
+- **Función de primera clase**: cualquier función que se puede guardar en una variable
+- **Función de orden superior**: los parámetros que recibe son otras funciones
+- **Función anónima**: que no tiene nombre
--- app.js
+++ app.js
@@ -42,3 +42,116 @@ const student = new Student('seba', 'sanchez', 'programacion')
 console.log({ student })
 console.log(student.hello())
 console.log(student.careerDatail())
+
+// función de primera clase
+function hello() {
+  console.log('Hola!')
+}
+const h = hello // guardando la función en una variable
+h() // ejecutando la función
+
+// con parámetros
+function sum(a, b) {
+  return a + b
+}
+const s = sum
+console.log(s(5, 5))
+
+// función de orden superior
+function some(fn) {
+  fn()
+}
+some(h)
+
+// con parámetros adicionales
+function some2(fn, num1, num2) {
+  const result = fn(num1, num2)
+  console.log('El resultado es:', result)
+}
+some2(s, 4, 4)
+
+// función anónima: tradicional
+some2(
+  function (a, b) {
+    return a * b
+  },
+  2,
+  3
+)
+
+// función anónima: arrow function
+some2((a, b) => a * b, 5, 5)
+
+// mutable (arreglo): modificando el arreglo original
+const numbers = [1, 2, 3, 4, 5]
+function mutable(collection, num) {
+  for (let i = 0; i < collection.length; i++) {
+    collection[i] *= num
+  }
+  return collection
+}
+
+const newArray = mutable(numbers, 2)
+console.log({ newArray, numbers })
+
+// inmutable (arreglo): el arreglo original no se altera
+const numbers2 = [1, 2, 3, 4, 5]
+function inmutable(collection, num) {
+  const newNumbers = []
+  for (const item of collection) {
+    newNumbers.push(item * num)
+  }
+  return newNumbers
+}
+
+const newArray2 = inmutable(numbers2, 3)
+console.log({ newArray2, numbers2 })
+
+// mutable (objectos)
+class Beer {
+  constructor(name, price) {
+    this.name = name
+    this.price = price
+  }
+}
+
+function toUpperMutable(beer) {
+  beer.name = beer.name.toUpperCase()
+  return beer
+}
+
+const beer = new Beer('cerveza', 10)
+const newBeer = toUpperMutable(beer)
+console.log({ beer, newBeer })
+
+// inmutable (objectos)
+function toUpperInmutable(beer) {
+  // creando una copia con el operador spread (no recomendada para objectos complejos)
+  // const newBeer = { ...beer }
+  const newBeer = structuredClone(beer)
+  newBeer.name = newBeer.name.toUpperCase()
+  return newBeer
+}
+
+const beer2 = new Beer('cerveza', 10)
+const newBeer2 = toUpperInmutable(beer2)
+console.log({ beer2, newBeer2 })
+
+// búsqueda: modo estructurado
+const names = ['Belu', 'Seba', 'Valen', 'Ramsito', 'Rossana']
+function searchFirstLetter(collection, letter) {
+  const newCollection = []
+  for (const word of collection) {
+    if (word[0].toLowerCase() === letter.toLowerCase()) {
+      newCollection.push(word)
+    }
+  }
+  return newCollection
+}
+console.log(searchFirstLetter(names, 'r'))
+
+// búsqueda: con método inmutable que ofrece JS
+function searchFirstLetter2(collection, letter) {
+  return collection.filter((word) => word[0].toLowerCase() === letter.toLowerCase())
+}
+console.log(searchFirstLetter2(names, 'r'))
